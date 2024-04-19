7ce11f36045e33db132506e39a92ca809c096d8a
Bruce Fanger Jr <140328124+Fanger1985@users.noreply.github.com>
2024-04-18 22:58:27 -0400

    Live Video ESP32 Streaming
    this just streams to an ip address : for this application i flipped the x & y due to how its currently mounted on the robot.  This script just streams video to an ip on the local network.

---
+++ AprilBBRemoteCam.ino
@@ -0,0 +1,89 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+//
+// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+//            Partial images will be transmitted if image exceeds buffer size
+//
+
+// Select camera model
+#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+
+#include "camera_pins.h"
+
+const char* ssid     = "SpectrumSetup-DD";   //input your wifi name
+const char* password = "jeansrocket543";   //input your wifi passwords
+
+void startCameraServer();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 10;  // Lower quality for faster processing
+    config.fb_count = 1;  // Less frame buffers to handle
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_vflip(s, 1);  // Flip the camera image vertically
+  s->set_hmirror(s, 1);  // Flip the camera image horizontally
+  s->set_framesize(s, FRAMESIZE_VGA);  // Consider lower resolution if still choppy
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void loop() {
+  // Remove the delay or reduce it for a faster refresh rate
+  delay(100);  // Reduced delay for more frequent updates
+}
+
+
+
